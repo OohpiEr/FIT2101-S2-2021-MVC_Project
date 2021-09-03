7a965998dfecf52e934ef3fa277b869c80866b05
wpan0017 <weihung0801@gmail.com>
2021-09-03 20:27:03 +0800

    Author: Wei Hung changes: 1) updated server.js

--- application/server.js
+++ application/server.js
@@ -6,9 +6,53 @@
 const http = require('http');
 const app = require('./backend/app');
 
-const port = process.env.PORT || 3000;
+const normalizePort = val => {  // server port
+  var port = parseInt(val, 10);
 
-app.set("port", port)       // inform express which port currently working on
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+// Check which type of error occurs
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + port;
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);          // inform express which port currently working on
+
 const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);    //log we are listenting the address
+server.listen(port);
 
-server.listen(port);        // server port
+// installed nodemon to avoid constantly restarting server
