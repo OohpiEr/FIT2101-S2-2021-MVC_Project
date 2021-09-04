cf0f63ff90d4deab84183073e559f22c1a0a4604
Bryan Lim <clim0100@student.monash.edu>
2021-09-04 10:59:36 +0800

    NavBar update

--- app/frontend/src/App.vue
+++ app/frontend/src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="content">
+      <div class="top-bar">
+        <Navigation />
+      </div>
+      <img alt="Donald Duck" src="./assets/Donald.png" style="width:300px;height:300px">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Navigation from './components/Navigation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Navigation
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: relative;
+  width: calc(100% - 20px);
+  height: calc(100vh - 20px);
+  padding: 10px;
+  color: #333;
+  overflow: hidden;
+}
+
+.content{
+  position: absolute;
+  top: 10px;
+  width: calc(100% - 60px);
+  height: calc(100vh - 60px);
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 30px;
+  box-shadow: 2px 2px 2px 1px rgba(0, 0, 0, 0.2);
+  transition: 1s transform cubic-bezier(0,.12,.14,1);
+}
+
+.top-bar{
+  display:flex;
+  width: 100%;
 }
 </style>
