5d19554716f5d5073f1675012afd5a2db3f6c017
Bryan Lim <clim0100@student.monash.edu>
2021-09-04 11:19:04 +0800

    experimental mpa spike

---
+++ app/frontend/vue.config.js
@@ -0,0 +1,11 @@
+module.exports ={
+    pages:{
+        analysis:{
+            entry: 'src/analysis/main.js',
+            template: 'public/index.html',
+            filename: 'employee.html',
+            title: 'Employee',
+            chunks: ['chunk-vendors', 'chunk-common', 'employee']
+        }
+    }
+}
