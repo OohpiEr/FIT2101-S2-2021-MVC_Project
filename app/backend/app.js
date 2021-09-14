28d75c683c3ae67d9cbcfe59b2eac6585ad5df45
wpan0017 <wpan0017@student.monash.edu>
2021-09-14 17:57:04 +0800

    [5,6] login&signup feature
    changes:
    1) added token for authentication verification
    2) created a random generated function for signup via backend to test feature

--- app/backend/app.js
+++ app/backend/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
-
 const userRoutes = require('./routes/user');
 
 const app = express();
--- app/backend/middleware/check-auth.js
+++ app/backend/middleware/check-auth.js
@@ -3,6 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req,res,next) => {
     try {
         const token = req.headers.authorisation.split(" ")[1];   // try to check if user action was authorised
+        jwt.verify(token, 'long_secret_key_that_should_be_used_for_authentication');
+        next();
     } catch (error) {
         res.status(401).json({
             message: "Auth failed!"
