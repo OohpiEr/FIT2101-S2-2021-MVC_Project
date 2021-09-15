79ec1874b2a2aa798a429190deb467ae30b214b8
Sami <shus0013@student.monash.edu>
2021-09-15 13:52:00 +0400

    Fixed api.js so that the functions can be properly improted
    Exported all the functions in the api.js

--- app/backend/api.js
+++ app/backend/api.js
@@ -3,32 +3,21 @@ const url = 'https://api.covid19api.com';
 
 
 // Get detailed covid data for a country (can be modified to include time intervals)
-async function fetchCountryData(country){
+export async function fetchCountryData(country){
     const response = await axios.get(`${url}/total/country/${country}/status/confirmed`);
     return response.data;
       
 }
 
 // Get general information for all the countries
-async function fetchCountries(){
+export async function fetchCountries(){
     const response = await axios.get(`${url}/summary`);
     return response.data.Countries;
 }
 
 
 // General world wide covid data summary
-async function fetchGlobal(){
+export async function fetchGlobal(){
     const response = await axios.get(`${url}/summary`);
     return response.data.Global;
 }
-
-// test run    
-async function run()
-{   
-    // testing the fetch functions
-    let data = await fetchCountryData("France");
-    console.log(data);
-    //let data = await fetchCountries();
-    //console.log(data);
-}
-run();
