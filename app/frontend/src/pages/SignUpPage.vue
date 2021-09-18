9b50a8834a43d1d499cc168c85cb22fc32492651
OohpiEr <90300732+OohpiEr@users.noreply.github.com>
2021-09-18 17:27:48 +0800

    (6) User story: As a client, I want a signup feature so that the app can be offered as a subscription service to the public (added form control and validation to password)

--- app/frontend/src/pages/SignUpPage.vue
+++ app/frontend/src/pages/SignUpPage.vue
@@ -6,66 +6,87 @@
       <form @submit.prevent="handleSubmit">
         <div class="row">
           <div class="col-md-12">
-            <base-input
-              label="Username"
-              placeholder="Username"
-              v-model="user.username"
-              required
-            >
-            </base-input>
+            <div class="form-group">
+              <label for="user.password">Username</label>
+              <input
+                v-model="user.username"
+                placeholder="mike123"
+                v-bind:class="{
+                  'form-control': true,
+                }"
+                required
+              />
+            </div>
           </div>
         </div>
 
         <div class="row">
           <div class="col-md-12">
-            <base-input
-              id="input"
-              label="Email address"
-              type="email"
-              placeholder="mike@email.com"
-              v-model="user.email"
-              required
-            >
-            </base-input>
+            <div class="form-group">
+              <label for="user.password">Email address</label>
+              <input
+                type="email"
+                placeholder="mike@email.com"
+                v-model="user.email"
+                v-bind:class="{
+                  'form-control': true,
+                }"
+                required
+              />
+            </div>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="col-md-12">
+            <div class="form-group">
+              <label for="user.password">Phone Number</label>
+              <input
+                v-model="user.phone"
+                v-bind:class="{
+                  'form-control': true,
+                }"
+                required
+              />
+            </div>
           </div>
         </div>
 
         <div class="row">
           <div class="col-md-12">
-            <base-input
-              id="input"
-              label="Phone Number"
-              placeholder="0123456789"
-              v-model="user.phone"
-              required
-            >
-            </base-input>
+            <div class="form-group">
+              <label for="user.password">Password</label>
+              <input
+                type="password"
+                v-model="user.password"
+                v-bind:class="{
+                  'form-control': true,
+                  'is-invalid': !confirmPassword(),
+                }"
+                required
+              />
+            </div>
           </div>
         </div>
 
         <div class="row">
           <div class="col-md-12">
-            <base-input
-              id="input"
-              label="Password"
-              type="password"
-              v-model="user.password"
-              required
-            >
-            </base-input>
-          </div>
-        </div>
-
-        <div class="row">
-          <div class="col-md-12">
-            <base-input
-              id="input"
-              label="Confirm Password"
-              type="password"
-              v-model="user.confirmPassword"
-              required
-            >
-            </base-input>
+            <div class="form-group">
+              <label for="user.confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                v-model="user.confirmPassword"
+                v-bind:class="{
+                  'form-control': true,
+                  'is-invalid': !confirmPassword(),
+                }"
+                required
+              />
+              <div class="invalid-feedback">
+                Confirm password must be the same as password
+              </div>
+            </div>
           </div>
         </div>
 
@@ -86,7 +107,7 @@
 
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "sign-up",
@@ -99,6 +120,8 @@ export default {
         password: "",
         confirmPassword: "",
       },
+      valid: false,
+      submitted: false,
     };
   },
   methods: {
@@ -107,23 +130,38 @@ export default {
 
       // TODO password validation
 
-      const signup = {
-        useremail: this.user.email,
-        username: this.user.username,
-        contact: this.user.phone,
-        password: this.user.password,
-      };
-      axios
-        .post("http://localhost:3000/api/user/signup", signup)
-        .then((response) => {
-          console.log(response);
-          // TODO add feedback here -> SUCCESS (201)
-        })
-        .catch((error) => {
-          console.log(error);
-          // TODO add feedback here -> ERROR (500 - email ady exists)
-        });
+      // const signup = {
+      //   useremail: this.user.email,
+      //   username: this.user.username,
+      //   contact: this.user.phone,
+      //   password: this.user.password,
+      // };
+      // axios
+      //   .post("http://localhost:3000/api/user/signup", signup)
+      //   .then((response) => {
+      //     console.log(response);
+      //     // TODO add feedback here -> SUCCESS (201)
+      //   })
+      //   .catch((error) => {
+      //     console.log(error);
+      //     // TODO add feedback here -> ERROR (500 - email ady exists)
+      //   });
     },
+    validate: function () {
+      if (this.confirmPassword()) {
+        this.valid = true;
+      }
+    },
+    confirmPassword: function () {
+      return this.user.password == this.user.confirmPassword;
+    },
+    submit: function () {
+      this.validate();
+      if (this.valid) {
+        //THIS IS WHERE YOU SUBMIT DATA TO SERVER
+        this.submitted = true;
+      }
+    }, //end submit
   },
 };
 </script>
@@ -139,4 +177,8 @@ base-input {
 .button {
   padding: 15px;
 }
+
+/* .error-msg{
+  color: red;
+} */
 </style>
