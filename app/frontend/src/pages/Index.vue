d520543e3594900d659eef0d8dbb92e14cd562b5
Jason The <jthe0005@student.monash.edu>
2021-09-19 15:24:42 +0700

    (16) Make html page. Added image, table, texts

--- app/frontend/src/pages/Index.vue
+++ app/frontend/src/pages/Index.vue
@@ -1,13 +1,50 @@
 <template>
+  <div>
+    <div><img src="frontend/public/img/data_analysis.jpg"></div>
 
+    <div style="color: white">
+      2101_Data_Analytics: A versatile widget for variant Data Visualisation and
+      Analytics
+    </div>
+    <br />
+
+    <div style="color: white">
+      2101_Data_Analytics is a versatile widget based on a multi-layered MVC
+      that displays different types of data from different sources and produce
+      appropriate analysis and predictions. Unlike some other services, our
+      product adds value to our target group without collecting and utilising
+      users’ data for commercial purposes.
+    </div>
+
+    <div style="color: white; margin-top: 22px">
+      Choose your plan
+      <table>
+        <tr>
+          <th></th>
+          <th>Basic</th>
+        </tr>
+        <tbody>
+          <tr>
+            <td>Monthly Price</td>
+            <td>RM 200</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <div style="color: white">
+      <button>Register</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import Table from "./Table.vue";
 export default {
-  name: "Index.vue"
-}
+  components: { Table },
+  name: "Index.vue",
+};
 </script>
 
 <style scoped>
-
 </style>
