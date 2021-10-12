c5d6e281388e07b8c6784b9a18ab07920f35a612
Shuta Gunraku <shuta.thomas.gunrak@gmail.com>
2021-10-12 09:55:34 +0900

    (28) Add Edit/Save button to update personal information (Modified css for inputs)

--- app/frontend/src/pages/Profile/EditProfileForm.vue
+++ app/frontend/src/pages/Profile/EditProfileForm.vue
@@ -10,93 +10,60 @@
         </base-input>
       </div> -->
       <div class="col-md-3 pr-md-1">
-        <base-input label="Username"
-                  placeholder="Username"
-                  v-model="model.username">
+        <base-input
+          label="Username"
+          placeholder="Username"
+          v-model="model.username"
+          disabled
+        >
         </base-input>
       </div>
       <div class="col-md-5 px-md-1">
-        <base-input label="Email address"
-                  type="email"
-                  placeholder="mike@email.com"
-                  v-model="model.email">
+        <base-input
+          label="Email address"
+          type="email"
+          placeholder="mike@email.com"
+          v-model="model.email"
+          disabled
+        >
         </base-input>
       </div>
       <div class="col-md-4 pl-md-1">
-        <base-input label="Phone Number"
-                  placeholder="Company"
-                  v-model="model.phone">
+        <base-input
+          label="Phone Number"
+          placeholder="Company"
+          v-model="model.phone"
+        >
         </base-input>
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-6 pr-md-1">
-        <base-input label="First Name"
-                  v-model="model.firstName"
-                  placeholder="First Name">
-        </base-input>
-      </div>
-      <div class="col-md-6 pl-md-1">
-        <base-input label="Last Name"
-                  v-model="model.lastName"
-                  placeholder="Last Name">
-        </base-input>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-12">
-        <base-input label="Address"
-                  v-model="model.address"
-                  placeholder="Home Address">
-        </base-input>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-4 pr-md-1">
-        <base-input label="City"
-                  v-model="model.city"
-                  placeholder="City">
-        </base-input>
-      </div>
-      <div class="col-md-4 px-md-1">
-        <base-input label="Country"
-                  v-model="model.country"
-                  placeholder="Country">
-        </base-input>
-      </div>
-      <div class="col-md-4 pl-md-1">
-        <base-input label="Postal Code"
-                  placeholder="ZIP Code">
-        </base-input>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-md-8">
-        <base-input>
-          <label>About Me</label>
-          <textarea rows="4" cols="80"
-                    class="form-control"
-                    placeholder="Here can be your description"
-                    v-model="model.about">
+    <base-button name="edit_save_btn" slot="footer" type="primary" fill>Edit</base-button>
 
-              </textarea>
-        </base-input>
-      </div>
-    </div>
-    <base-button slot="footer" type="primary" fill>Save</base-button>
   </card>
 </template>
 <script>
-  export default {
-    props: {
-      model: {
-        type: Object,
-        default: () => {
-          return {};
-        }
-      }
-    }
-  }
+export default {
+  props: {
+    model: {
+      type: Object,
+      default: () => {
+        return {};
+      },
+    },
+  },
+};
 </script>
 <style>
+input {
+  background: white !important;
+  color: black !important;
+}
+input:disabled {
+  background: grey !important;
+  color: white !important;
+}
+.edit_button{
+  color: white !important;
+  background: white !important;
+}
 </style>
