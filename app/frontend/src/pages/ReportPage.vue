7b0d57bc8136a0e42b2a5928e8c96d26fa943d2e
OohpiEr <90300732+OohpiEr@users.noreply.github.com>
2021-10-12 13:40:34 +0800

    changed table column title and hidden vertical scroll for table

--- app/frontend/src/pages/ReportPage.vue
+++ app/frontend/src/pages/ReportPage.vue
@@ -1,66 +1,20 @@
 <template>
   <div class="row">
     <div class="col-12">
-      <card :title="table1.title">
+      <card :title="reportTable.title">
         <div class="table-responsive overflow-hidden">
           <base-table
-            :data="table1.data"
-            :columns="table1.columns"
+            :data="reportTable.data"
+            :columns="reportTable.columns"
             thead-classes="text-primary"
           >
             <template slot="columns">
-              <th v-for="column in columns" :key="column">{{ column }}</th>
+              <th v-for="column in reportTable.columnNames" :key="column">{{ column }}</th>
             </template>
           </base-table>
         </div>
       </card>
     </div>
-
-    <!-- <base-table :data="tableData" :columns="columns">
-      <template slot="columns">
-        <th class="text-center">#</th>
-        <th>Name</th>
-        <th>Job Position</th>
-        <th>Since</th>
-        <th class="text-right">Salary</th>
-        <th class="text-right">Actions</th>
-      </template>
-      <template slot-scope="{ row }">
-        <td>{{ row.id }}</td>
-        <td>{{ row.name }}</td>
-        <td>{{ row.job }}</td>
-        <td>{{ row.since }}</td>
-        <td>{{ row.salary }}</td>
-        <td class="td-actions text-right">
-          <base-button type="info" size="sm" icon>
-            <i class="tim-icons icon-single-02"></i>
-          </base-button>
-          <base-button type="success" size="sm" icon>
-            <i class="tim-icons icon-settings"></i>
-          </base-button>
-          <base-button type="danger" size="sm" icon>
-            <i class="tim-icons icon-simple-remove"></i>
-          </base-button>
-        </td>
-      </template>
-    </base-table>
-
-    <div class="row">
-      <div class="col-12">
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th v-for="col in table1.columns" scope="col">{{ col }}</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(country, index) in table1.data">
-              <td v-for="data in country">{{ data }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div> -->
   </div>
 </template>
 <script>
@@ -74,65 +28,7 @@ const tableColumns = [
   "login_count",
   "last_login",
 ];
-// const tableColumns = ["username", "useremail", "Phone", "Number Of Logins", "Last Login"];
-// const tableData = [
-//   {
-//     id: 1,
-//     username: "Dakota Rice",
-//     login_count: "$36.738",
-//     useremail: "Niger",
-//     contact: "Oud-Turnhout",
-//     last_login: "Oud-Turnhout",
-//   },
-//   {
-//     id: 2,
-//     username: "Minerva Hooper",
-//     login_count: "$23,789",
-//     useremail: "Curaçao",
-//     contact: "Sinaai-Waas",
-//     last_login: "Oud-Turnhout",
-//   },
-//   {
-//     id: 3,
-//     username: "Sage Rodriguez",
-//     login_count: "$56,142",
-//     useremail: "Netherlands",
-//     contact: "Baileux",
-//     last_login: "Oud-Turnhout",
-//   },
-//   {
-//     id: 4,
-//     username: "Philip Chaney",
-//     login_count: "$38,735",
-//     useremail: "Korea, South",
-//     contact: "Overland Park",
-//     last_login: "Oud-Turnhout",
-//   },
-//   {
-//     id: 5,
-//     username: "Doris Greene",
-//     login_count: "$63,542",
-//     useremail: "Malawi",
-//     contact: "Feldkirchen in Kärnten",
-//     last_login: "Oud-Turnhout",
-//   },
-//   {
-//     id: 6,
-//     username: "Mason Porter",
-//     login_count: "$98,615",
-//     useremail: "Chile",
-//     contact: "Gloucester",
-//     last_login: "Oud-Turnhout",
-//   },
-//   {
-//     id: 7,
-//     username: "Jon Porter",
-//     login_count: "$78,615",
-//     useremail: "Portugal",
-//     contact: "Gloucester",
-//     last_login: "Oud-Turnhout",
-//   },
-// ];
+const columnNames = ["username", "email", "Phone", "Number Of Logins", "Last Login"];
 
 export default {
   components: {
@@ -140,9 +36,10 @@ export default {
   },
   data() {
     return {
-      table1: {
+      reportTable: {
         title: "Report",
         columns: [...tableColumns],
+        columnNames: [...columnNames],
         data: [],
       },
     };
@@ -164,14 +61,13 @@ export default {
     for (let i = 0; i < tableData.length; i++) {
       var date = new Date(tableData[i].last_login);
       tableData[i].last_login =date.toLocaleDateString("en-MY", options);
-      console.log(tableData[i].last_login); 
     }
-    this.table1.data = tableData;
+    this.reportTable.data = tableData;
   },
 };
 </script>
 <style>
-/* .overflow-hidden {
-  overflow: hidden;
-} */
+.overflow-hidden {
+     overflow-y: hidden;  
+}
 </style>
