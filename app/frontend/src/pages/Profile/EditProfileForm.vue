ad0f437745e9546a6c9104770ca8d6dfe2e3e8b2
Shuta Gunraku <shuta.thomas.gunrak@gmail.com>
2021-10-13 22:44:46 +0900

    (28) Add Edit/Save button to update personal information (Button dynamic, allow changes for username and phone number)

--- app/frontend/src/pages/Profile/EditProfileForm.vue
+++ app/frontend/src/pages/Profile/EditProfileForm.vue
@@ -2,15 +2,9 @@
   <card>
     <h5 slot="header" class="title">Edit Profile</h5>
     <div class="row">
-      <!-- <div class="col-md-5 pr-md-1">
-        <base-input label="Company (disabled)"
-                  placeholder="Company"
-                  v-model="model.company"
-                  disabled>
-        </base-input>
-      </div> -->
       <div class="col-md-3 pr-md-1">
         <base-input
+          id="username"
           label="Username"
           placeholder="Username"
           v-model="model.username"
@@ -20,28 +14,34 @@
       </div>
       <div class="col-md-5 px-md-1">
         <base-input
+          id="email"
           label="Email address"
           type="email"
           placeholder="mike@email.com"
           v-model="model.email"
-          disabled
+          disabled="none"
         >
         </base-input>
       </div>
       <div class="col-md-4 pl-md-1">
         <base-input
+          id="phone_number"
           label="Phone Number"
           placeholder="Company"
           v-model="model.phone"
+          disabled
         >
         </base-input>
       </div>
     </div>
-    <base-button name="edit_save_btn" slot="footer" type="primary" fill>Edit</base-button>
+    <base-button id="edit_save_btn" slot="footer" type="primary" fill @click = "modify_button">Edit</base-button>
+
 
   </card>
 </template>
 <script>
+import NotificationTemplate from "../Notifications/NotificationTemplate";
+
 export default {
   props: {
     model: {
@@ -51,7 +51,25 @@ export default {
       },
     },
   },
+  methods: {
+    modify_button() {
+      let btn = document.getElementById("edit_save_btn");
+      let username = document.getElementById("username");
+      let phone_number = document.getElementById("phone_number");
+      if (btn.innerText === "Edit"){
+        btn.innerText = "Save";
+        username.removeAttribute("disabled");
+        phone_number.removeAttribute("disabled");
+      }
+      else{
+        btn.innerText = "Edit";
+        username.disabled = "true";
+        phone_number.disabled = "true";
+      }
+    }
+  }
 };
+
 </script>
 <style>
 input {
@@ -65,5 +83,10 @@ input:disabled {
 .edit_button{
   color: white !important;
   background: white !important;
+}
+#edit_save_btn{
+  width: 100px;
+  padding-left: 20px;
+  padding-right: 20px;
 }
 </style>
