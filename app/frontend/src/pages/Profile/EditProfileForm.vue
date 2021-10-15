0e51733a8a8ea80a3b0a25230fff72478e823f5b
Jason The <jthe0005@student.monash.edu>
2021-10-15 20:29:23 +0700

    (29) Perform password validation and remove unnecessary comments

--- app/frontend/src/pages/Profile/EditProfileForm.vue
+++ app/frontend/src/pages/Profile/EditProfileForm.vue
@@ -46,11 +46,9 @@
       >
     </card>
     <card>
-      <!---->
       <h5 slot="header" class="title">Change Password</h5>
       <div class="row">
         <div class="col-md-4 pr-md-1">
-          <!--v-model="model.oldpassword"-->
           <base-input
             id="old_password"
             label="Old Password"
@@ -64,18 +62,12 @@
       </div>
       <div class="row">
         <div class="col-md-4 pr-md-1">
-          <!--v-model="model.newpassword"-->
           <base-input
             id="new_password"
             label="New Password"
             v-model="model.newpassword"
             placeholder="New Password"
             type="password"
-            v-bind:class="{
-              'is-invalid': !(
-                this.model.newpassword == this.model.confirmpassword
-              ),
-            }"
             disabled
           >
           </base-input>
@@ -83,25 +75,15 @@
       </div>
       <div class="row">
         <div class="col-md-4 pr-md-1">
-          <!--v-model="model.confirmpassword"-->
           <base-input
             id="confirm_password"
             label="Confirm Password"
             v-model="model.confirmpassword"
             placeholder="Confirm Password"
             type="password"
-            v-bind:class="{
-              'is-invalid': !(
-                this.model.newpassword == this.model.confirmpassword
-              ),
-            }"
             disabled
           >
           </base-input>
-          <div class="invalid-feedback" style="color: red">
-            ignore this--------------Confirm password must be the same as
-            password
-          </div>
         </div>
       </div>
 
@@ -114,8 +96,6 @@
         Confirm password must be the same as new password!
       </base-alert>
 
-      <!-- @click="modify_button" -->
-      <!-- @click="modify_password" -->
       <base-button
         id="edit_change_password_btn"
         slot="footer"
@@ -159,8 +139,7 @@ export default {
           username: this.model.username,
           contact: this.model.phone,
         };
-        console.log(update); // this one prints in console
-        // {useremail: 'Test18@gmail.com', username: 'AE', contact: '012222'}
+        console.log(update);
         const response = await axios.put(
           "http://localhost:3000/api/user/update/info",
           update,
@@ -178,16 +157,11 @@ export default {
     },
 
     async modify_password() {
-      // need to hide the password when typed, but will do this after everything runs well
       let change_button = document.getElementById("edit_change_password_btn");
-      let username = document.getElementById("username");
 
       let old_password = document.getElementById("old_password");
       let new_password = document.getElementById("new_password");
       let confirm_password = document.getElementById("confirm_password");
-      // old_password.value = "";
-      // new_password.value = "";
-      // confirm_password.value = "";
 
       if (change_button.innerText === "Change Password") {
         change_button.innerText = "Save Password";
@@ -202,24 +176,13 @@ export default {
         old_password.disabled = "true";
         new_password.disabled = "true";
         confirm_password.disabled = "true";
-        // old_password.value = "";
-        // new_password.value = "";
-        // confirm_password.value = "";
 
         const update_password = {
-          useremail: this.model.email, // cannot get email
+          useremail: this.model.email,
           newpassword: this.model.newpassword,
           oldpassword: this.model.password,
-          // this.model.newpassword == this.model.confirmpassword ? <proceed> : <ask user to check>
-          // how to raise error to the screen? "New Password is not the same as Confirm Password"
-          // think they have the functionality on other component?
-          // need to compare new password and confirm password
         };
-        // confirmpassword: this.model.confirmpassword,
-        console.log(update_password); // this one prints in console
-        // this is the things that the user typed into the fields
-        // example from personal information
-        // {useremail: 'Test18@gmail.com', username: 'AE', contact: '012222'}
+        console.log(update_password);
 
         if (this.model.newpassword == this.model.confirmpassword) {
           this.newPasswordValidationFail = false;
@@ -228,54 +191,19 @@ export default {
             update_password,
             {
               headers: {
-                Authorization: localStorage.getItem("token"), // Wei Hung asked this for testing purpose
+                Authorization: localStorage.getItem("token"),
               },
             }
           );
           console.log(response_password);
-          // for error is 401?
           if (response_password.status === 200) {
             localStorage.setItem("userdata", JSON.stringify(update_password));
           }
         } else if (this.model.newpassword != this.model.confirmpassword) {
           this.newPasswordValidationFail = true;
-          //old_password.value = "";
-          //new_password.value = "";
-          //confirm_password.value = "";
         }
       }
     },
-
-    // async modify_password() {
-    //   console.log("attempting to change password");
-
-    //   const postinfo = {
-    //     // useremail: this.user.email,
-    //     // password: this.user.password,
-    //     useremail: this.model.email,
-    //     newpassword: this.model.newpassword,
-    //     oldpassword: this.model.password,
-    //   };
-    //   axios
-    //     .post("http://localhost:3000/api/user/update/pwd", postinfo)
-    //     .then((response) => {
-    //       // this.loginFail = false;
-    //       // this.loginSuccess = true;
-    //       localStorage.setItem("token", response.data.token);
-    //       localStorage.setItem("userdata", JSON.stringify(response.data));
-    //       // store.user.username = response.data.username;
-    //       // store.user.email = response.data.useremail;
-    //       // store.token = response.data.token;
-    //       //router.push("dashboard");
-    //       console.log(response.data);
-    //     })
-    //     .catch((error) => {
-    //       console.log(error.message);
-    //       // if (!this.loginFail) {
-    //       //   this.loginFail = true;
-    //       // }
-    //     });
-    // },
   },
 };
 </script>
